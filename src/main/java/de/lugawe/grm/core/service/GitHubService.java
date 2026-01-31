082a425c8927e6fc9a0bce4261ae30b3324503c8
Luis Weich <luis.gabriel.weich@gmail.com>
2026-01-31 23:08:52 +0100

    GitHubService throws Exception

--- src/main/java/de/lugawe/grm/core/service/GitHubService.java
+++ src/main/java/de/lugawe/grm/core/service/GitHubService.java
@@ -6,11 +6,11 @@ import de.lugawe.grm.core.domain.Release;
 
 public interface GitHubService {
 
-    List<Release> getReleases(String repository);
+    List<Release> getReleases(String repository) throws Exception;
 
-    Release getRelease(String repository, String releaseName);
+    Release getRelease(String repository, String releaseName) throws Exception;
 
-    default Release getLatestRelease(String repository) {
+    default Release getLatestRelease(String repository) throws Exception {
         return getRelease(repository, Release.LATEST);
     }
 }
